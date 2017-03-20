12cc4d7b2cedc516f81f4c2ff9f7611001484961
bbui1997 <bbui1997@gmail.com>
2017-03-20 00:46:09 -0400

    Bug fixes, added error handling when taking a turn, did small refactoring (don't have to set game to player and add player to game, we did bidirectionality for a reason)

--- mancala/src/main/java/GameClient.java
+++ mancala/src/main/java/GameClient.java
@@ -1,13 +1,16 @@
 /**
  * Created by Jess on 3/19/2017.
  */
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
+import java.util.Scanner;
+
 public class GameClient {
 
     public static Scanner scan;
@@ -22,7 +25,7 @@ public class GameClient {
     public static String address = "";
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //prepare to receive user input
         scan = new Scanner(System.in);
         if (args.length > 0) {
@@ -33,7 +36,7 @@ public class GameClient {
         //Game Splash, Player Selection, Host/Client selection
         boolean done = false;
         isHost = false;
-        while(!done) {
+        while (!done) {
             System.out.println("Welcome to Mancala!");
             System.out.println("Player 1 (Host) or Player 2 (Guest)?");
             String s = scan.next();
@@ -56,12 +59,12 @@ public class GameClient {
         showInstructions();
         //host/client setup logic
         try {
-           sock = isHost ? setupHost() : setupClient();
-           //I.O setup
-           out = new PrintWriter(sock.getOutputStream(), true);
-           in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-           //network issues
-        }catch (Exception e){
+            sock = isHost ? setupHost() : setupClient();
+            //I.O setup
+            out = new PrintWriter(sock.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            //network issues
+        } catch (Exception e) {
             System.out.println("Network issue. Cannot resolve." + e.toString());
             return;
         }
@@ -75,25 +78,22 @@ public class GameClient {
         p1.setGame(game);
         p2.setName("Client");
         p2.setGame(game);
-        game.addPlayer(p1);
-        game.addPlayer(p2);
-        System.out.println(isHost ? "You are Player 1":"You are Player 2");
+        System.out.println(isHost ? "You are Player 1" : "You are Player 2");
 
         //Game loop
-        while(!gameOver()){
+        while (!gameOver()) {
             printBoard();
-            if((game.getPlayerTurn() == 0 && isHost) || (game.getPlayerTurn() == 1 && !isHost)){
+            if ((game.getPlayerTurn() == 0 && isHost) || (game.getPlayerTurn() == 1 && !isHost)) {
                 takeTurn();
-            }
-            else{
+            } else {
                 System.out.println("The opponent is taking a turn");
                 receiveTurn();
             }
         }
-
+        scan.close();
     }
 
-    public static Socket setupHost() throws Exception{
+    public static Socket setupHost() throws Exception {
         //setup host logic
         int backlog = 3;
         InetAddress addr = InetAddress.getLocalHost();
@@ -102,13 +102,12 @@ public class GameClient {
         return serv.accept();
     }
 
-    public static Socket setupClient() throws Exception{
+    public static Socket setupClient() throws Exception {
         //setup client logic
-        if(!address.equals("")){
+        if (!address.equals("")) {
             System.out.println("CLIENT: connecting to addr:port " + address + ":" + port);
-            return new Socket(address,port);
-        }
-        else {
+            return new Socket(address, port);
+        } else {
             InetAddress addr = InetAddress.getLocalHost();
             System.out.println("CLIENT: connecting to addr:port " + addr + ":" + port);
             return new Socket(addr, port);
@@ -117,62 +116,72 @@ public class GameClient {
     }
 
     //Print the state of the board to the use (Command Line GUI)
-    public static void printBoard(){
+    public static void printBoard() {
         //Top half of the board
-        StringBuilder s = new StringBuilder("[P1] | " );
+        StringBuilder s = new StringBuilder("[P1] | ");
         s.append(flipP1(game.getP1Houses()));
         s.append("| \n ");
-        s.append(String.format("%2d",game.getP1Store().getPebbles())); //accounting for 2 digits
+        s.append(String.format("%2d", game.getP1Store().getPebbles())); //accounting for 2 digits
         s.append("  |             |  ");
-        s.append(String.format("%2d",game.getP2Store().getPebbles())); //accounting for 2 digits
+        s.append(String.format("%2d", game.getP2Store().getPebbles())); //accounting for 2 digits
         s.append("\n     | ");
         s.append(printP2(game.getP2Houses()));
         s.append("| [P2]");
         System.out.println(s.toString());
     }
 
-    public static String flipP1(List<House> l){
+    public static String flipP1(List<House> l) {
         String s = "";
-        for (House h:l){
+        for (House h : l) {
             s = h.getPebbles() + " " + s;
         }
         return s;
     }
-    public static String printP2(List<House> l){
+
+    public static String printP2(List<House> l) {
         String s = "";
-        for (House h:l){
+        for (House h : l) {
             s += h.getPebbles() + " ";
         }
         return s;
     }
 
-    public static void takeTurn(){
+    public static void takeTurn() {
         System.out.println("Your Turn");
         //handles invalid moves
-       boolean done = false;
+        boolean done = false;
         int i = 0;
-       while(!done) {
-           //Player picks a house
-           System.out.println("Choose a house 0-5 to redistribute:");
-           i = isHost ? 5-scan.nextInt():scan.nextInt();
-           if (i > 5 || i < 0){
-               System.out.println("CHOOSE A HOUSE 0-5!!!!!");
-               continue;
-           }
-           if (isHost) {
-               done = game.getP1Houses().get(i).redistributeCounterClockwise(); //take your turn
-           } else {
-               done = game.getP2Houses().get(i).redistributeCounterClockwise(); //take your turn
+        while (!done) {
+            //Player picks a house
+            System.out.println("Choose a house 0-5 to redistribute:");
+            try {
+                i = isHost ? 5 - scan.nextInt() : scan.nextInt();
+                if (i > 5 || i < 0) {
+                    System.out.println("CHOOSE A HOUSE 0-5!!!!!");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Try again.");
+                // gets rid of spam inputs
+                scan.nextLine();
+                printBoard();
+                takeTurn();
+            }
+            if (isHost) {
+                done = game.getP1Houses().get(i).redistributeCounterClockwise(); //take your turn
+            } else {
+                done = game.getP2Houses().get(i).redistributeCounterClockwise(); //take your turn
 
-           }
-       }
-      out.println(i);
+            }
+        }
+        out.println(i);
     }
-    public static void receiveTurn(){
+
+    public static void receiveTurn() {
         int i = 0;
         try {
             i = Integer.parseInt(in.readLine());
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error Receiving Move " + e.toString());
         }
         if (isHost) {
@@ -184,23 +193,29 @@ public class GameClient {
     }
 
     //check if game is over and who won
-    public static boolean gameOver(){
-        if(p1.hasWon()){
-            System.out.println(isHost ? "You Won!":"You Lost!");
-            return true;
-        }
-        if(p2.hasWon()){
-            System.out.println(isHost ? "You Won!":"You Lost!");
-            return true;
-        }
-        if(p1.hasTie()){
-            System.out.println("Tie!");
-            return true;
+    public static boolean gameOver() {
+        if (!game.canPlay()) {
+            System.out.println("\nGame Over!");
+            if (p1.hasWon()) {
+                printBoard();
+                System.out.println(isHost ? "You Won!" : "You Lost!");
+                return true;
+            }
+            if (p2.hasWon()) {
+                printBoard();
+                System.out.println(isHost ? "You Lost!" : "You Won!");
+                return true;
+            }
+            if (p1.hasTie()) {
+                printBoard();
+                System.out.println("Tie!");
+                return true;
+            }
         }
         return false;
     }
 
-    public static void showInstructions(){
+    public static void showInstructions() {
         System.out.println("Welcome to our Mancala Game");
         System.out.println("We are using a text-based GUI");
         System.out.println("You will see a board that looks like this:");
